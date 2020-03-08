90a9ccf954549cbf5140b387d7654a16f4f7997e
AndresHMosqueda <andreshm94@gmail.com>
2020-03-07 21:31:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+//crear el servidor
+const app = express();
+
+//Conectar a la Base de Datos
+conectarDB();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Definir la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola mundo!')
+})
+
+//arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+    
+})
